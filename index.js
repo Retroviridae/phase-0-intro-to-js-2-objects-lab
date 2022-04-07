cf6febc1a8d9181e295ecef04766d3e4566165b9
Alexander Schuessler <schuessler.alex@gmail.com>
2022-04-07 08:41:15 -0500

    commit tree 892467ad3abab52e795f0f86ebe0a60e683d569c

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your solution in this file!
+// let emp1 = {
+//     title: 'boss'
+// }
+// updateEmployeeWithKeyAndValue = (employee,key,value) => {
+//     return newEmployee = {
+//         ...employee,
+//         [key]: value,
+//     }
+// }
+// console.log(updateEmployeeWithKeyAndValue(emp1,'title','nerd'));
+// console.log(newEmployee)
+const gifts = ["teddy bear", "drone", "doll"];
+console.log(gifts.length);
